d3bb9619101e1e189325948e8c582d2d8363434a
IEEE-KJSIT <ieee.tech@somaiya.edu>
2025-03-27 19:25:21 +0530

    Update base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/leaderboard-standalone/',
+  base: '/algoforge.leaderboard/',
   server: {
     port: 5175 // Different from the main application port
   }
