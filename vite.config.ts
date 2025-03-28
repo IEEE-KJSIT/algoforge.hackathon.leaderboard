1d043d9cadafdeb8a156ac8d27b6ed0bd6b67454
IEEE-KJSIT <ieee.tech@somaiya.edu>
2025-03-28 16:07:48 +0530

    Update base URL to match new repository name

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/algoforge.leaderboard/',
+  base: '/algoforge.hackathon.leaderboard/',
   server: {
     port: 5175 // Different from the main application port
   }
